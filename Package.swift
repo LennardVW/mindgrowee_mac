74af14a2c719a10a1e5232e572a86c7852ac8507
LennardBot <lennard@mindgrowee.app>
2026-02-01 00:37:06 +0100

    Fix Package.swift - use macOS 14 instead of 15

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "mindgrowee_mac",
     defaultLocalization: "en",
-    platforms: [.macOS(.v15)],
+    platforms: [.macOS(.v14)],
     products: [
         .executable(name: "mindgrowee_mac", targets: ["mindgrowee_mac"])
     ],
